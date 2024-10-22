e31f0b5ad1bec7fc599df42b747cae9e103e3bd8
kasheesh <kashishbhurani@gmail.com>
2024-10-22 18:05:07 +0530

    Added django app

---
+++ game/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello world, you are at the wordle landing page")
+
+
+# Create your views here.
